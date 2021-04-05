e14f91c4dfb439233040e1d0bcbc87fea0b7fad8
User <serpodl@gmail.com>
2021-04-05 19:49:19 +0300

    Add tests for AliRadar website.

---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,43 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import drivers.CustomWebDriver;
+import io.qameta.allure.Step;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
+import static helpers.AttachmentsHelper.*;
+import static io.qameta.allure.Allure.step;
+
+public class TestBase {
+    @BeforeAll
+    static void setUpAll() {
+        addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
+        Configuration.browser = CustomWebDriver.class.getName();
+        Configuration.startMaximized = true;
+        Configuration.timeout = 10000;
+
+    }
+
+    @BeforeEach
+    void setUp() {
+        step("Открытие главной страницы вебсайта \"https://aliradar.com/\"", ()-> {
+            open("https://aliradar.com/");
+            sleep(5000);
+        });
+    }
+
+    @AfterEach
+    @Step("Attachments")
+    public void afterEach(){
+        attachScreenshot("Last screenshot");
+        attachPageSource();
+        attachAsText("Browser console logs", getConsoleLogs());
+        attachVideo();
+
+        closeWebDriver();
+    }
+}
